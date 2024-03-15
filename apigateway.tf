b27d644a5212a5e03c7c8a707ba36b15b3baf433
KaioAmaralB <kaio411@hotmail.com>
2024-03-14 22:01:22 -0300

    Ajustando API Gateway método

--- apigateway.tf
+++ apigateway.tf
@@ -54,7 +54,7 @@ resource "aws_api_gateway_resource" "auth_resource" {
 resource "aws_api_gateway_method" "auth_method" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.auth_resource.id
-  http_method   = "GET"
+  http_method   = "POST"
   authorization = "NONE"
 }
 
