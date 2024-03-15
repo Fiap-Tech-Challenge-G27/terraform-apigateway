0007d805323fc0b45789bb428123d8a87bec6cf5
KaioAmaralB <kaio411@hotmail.com>
2024-03-14 21:43:09 -0300

    Ajustando API Gateway método

--- apigateway.tf
+++ apigateway.tf
@@ -54,7 +54,7 @@ resource "aws_api_gateway_resource" "auth_resource" {
 resource "aws_api_gateway_method" "auth_method" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.auth_resource.id
-  http_method   = "POST"
+  http_method   = "GET"
   authorization = "NONE"
 }
 
@@ -63,7 +63,7 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   resource_id = aws_api_gateway_resource.auth_resource.id
   http_method = aws_api_gateway_method.auth_method.http_method
 
-  integration_http_method = "POST"
+  integration_http_method = "GET"
   type                    = "AWS_PROXY"
   uri                     = data.terraform_remote_state.lambda.outputs.lambda_function_invoke_arn
 }
